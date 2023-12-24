c21164ef10c2430fb05f5c6fb4484de9518c2f74
Mara <samaraore@gmail.com>
2023-12-23 21:47:57 -0500

    Added test player scene
    New player scene to test set up with a sprite animation. Created scene script that will add movement and world interaction

---
+++ player/sprite_test1.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+##this script will contain the following:
+##--adding movement in four directions
+##--adding an interaction action (figuring out how to make the player sprite
+##		interact with an object when the sprite gets close to it)
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#
+#
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += gravity * delta
+#
+#	# Handle jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
+#
